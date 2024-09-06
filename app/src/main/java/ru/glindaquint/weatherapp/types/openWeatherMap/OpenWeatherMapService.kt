cb27891ccb8a8bbf5a07ed84da252fb5b0032d87
glindaqu <q5481996@gmail.com>
2024-09-06 17:43:02 +0700

    metrics type change

--- app/src/main/java/ru/glindaquint/weatherapp/types/openWeatherMap/OpenWeatherMapService.kt
+++ app/src/main/java/ru/glindaquint/weatherapp/types/openWeatherMap/OpenWeatherMapService.kt
@@ -9,6 +9,7 @@ interface OpenWeatherMapService {
         @Query("q") cityName: String,
         @Query("appid") apiKey: String,
         @Query("lang") language: String = "ru",
+        @Query("units") unitsType: String = "metrics",
     ): OWMApiAnswer
 
     @GET("weather")
@@ -17,5 +18,6 @@ interface OpenWeatherMapService {
         @Query("lon") lon: Double,
         @Query("appid") apiKey: String,
         @Query("lang") lang: String = "ru",
+        @Query("units") unitsType: String = "metrics",
     ): OWMApiAnswer
 }
